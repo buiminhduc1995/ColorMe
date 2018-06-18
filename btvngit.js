5bbe94b7eeaca02a4edf2d6d78a0c0bc96292336
buiminhduc1995 <40017811+buiminhduc1995@users.noreply.github.com>
2018-06-18 14:31:54 +0700

    nopbaitapbanggit

---
+++ btvngit.js
@@ -0,0 +1,21 @@
+var sum=0
+const tinhtong= (a)=>{for (i=0;i<a;i++){
+	sum+=i*i;
+
+}console.log(sum)}
+tinhtong (3)
+const SUM= ()=>{
+    var SUM = 0; 
+    while (i >= 1) {
+        SUM += i;
+        if (SUM >= 1000) {
+            console.log(i);
+            break;
+        }
+        i++;
+    }
+}
+SUM()
+
+
+
